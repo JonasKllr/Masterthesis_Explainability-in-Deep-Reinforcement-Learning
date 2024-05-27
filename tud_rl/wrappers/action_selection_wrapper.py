152e91dda587722103d6ae1957b4e87b27a7be17
Jonas Keller <jonas.keller1@mailbox.tu-dresden.de>
2024-05-27 22:22:22 +0200

    cleaning up unused stuff

--- tud_rl/wrappers/action_selection_wrapper.py
+++ tud_rl/wrappers/action_selection_wrapper.py
@@ -6,18 +6,15 @@ from ixai.utils.wrappers.base import Wrapper
 class ActionSelectionWrapper(Wrapper):
     def __init__(
             self,
-            action_selection_function: typing.Union["Module", typing.Callable],
+            action_selection_function: typing.Callable,
             feature_names: typing.Optional[list] = None,
-            device: str = 'cpu'
             ):
         """
         Args:
-            link_function (Union[torch.nn.Module, Callable]): The function linking from the model input to the output.
-            device: (str): Torch device flag where the model is running. Defaults to `'cpu'`.
+            action_selection_function: (Callable): The function linking from the model input to the output.
             feature_names (list[str], optional): A ordered list of feature names what features should be provided.
         """
         super().__init__(action_selection_function, feature_names)
-        self._device: str = device
 
     def __call__(self, x: typing.Union[typing.List[dict], dict]) -> typing.Union[dict, typing.List[dict]]:
         """
@@ -32,10 +29,3 @@ class ActionSelectionWrapper(Wrapper):
         x_input = self.convert_1d_input_to_arr(x)
         output = self._prediction_function(x_input)
         return self.convert_arr_output_to_dict(output)
-        
-
-        # x_input = self.convert_2d_input_to_arr(x)
-        # x_input = torch.tensor(x_input, device=self._device, dtype=torch.float32)
-        # y_predictions = self._prediction_function(x_input).detach().cpu().numpy()
-        # y_prediction = [self.convert_arr_output_to_dict(y_predictions[i]) for i in range(len(y_predictions))]
-        # return y_prediction
