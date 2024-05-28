ab5ccaa7ce431473bc704b95f23a2808de4b2968
Jonas Keller <jonas.keller1@mailbox.tu-dresden.de>
2024-05-28 13:52:06 +0200

    formatted

--- tud_rl/wrappers/action_selection_wrapper.py
+++ tud_rl/wrappers/action_selection_wrapper.py
@@ -5,10 +5,10 @@ from ixai.utils.wrappers.base import Wrapper
 
 class ActionSelectionWrapper(Wrapper):
     def __init__(
-            self,
-            action_selection_function: typing.Callable,
-            feature_names: typing.Optional[list] = None,
-            ):
+        self,
+        action_selection_function: typing.Callable,
+        feature_names: typing.Optional[list] = None,
+    ):
         """
         Args:
             action_selection_function: (Callable): The function linking from the model input to the output.
@@ -16,7 +16,9 @@ class ActionSelectionWrapper(Wrapper):
         """
         super().__init__(action_selection_function, feature_names)
 
-    def __call__(self, x: typing.Union[typing.List[dict], dict]) -> typing.Union[dict, typing.List[dict]]:
+    def __call__(
+        self, x: typing.Union[typing.List[dict], dict]
+    ) -> typing.Union[dict, typing.List[dict]]:
         """
         Args:
             x (Union[list[dict], dict]): Input features in the form of a dict (1d-input) mapping from feature names to
@@ -25,7 +27,7 @@ class ActionSelectionWrapper(Wrapper):
         Returns:
             (Union[list[dict], dict]): The model output as a dictionary following river conventions.
         """
-        
+
         x_input = self.convert_1d_input_to_arr(x)
         output = self._prediction_function(x_input)
         return self.convert_arr_output_to_dict(output)
