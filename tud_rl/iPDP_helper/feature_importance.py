41a45296406a23e983efaaf43dab648ce2bcd7a9
Jonas Keller <jonas.keller1@mailbox.tu-dresden.de>
2024-06-07 13:42:21 +0200

    plotting of feature importance bar chart refactored in function

--- tud_rl/iPDP_helper/feature_importance.py
+++ tud_rl/iPDP_helper/feature_importance.py
@@ -1,4 +1,5 @@
 import math
+import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -13,3 +14,15 @@ def calculate_feature_importance(pdp_x: dict, pdp_y: dict) -> np.float32:
 
     # standard deviation for samples casted to float32
     return np.float32(math.sqrt(1 / (n - 1) * sum_individual_distance))
+
+
+def plot_feature_importance(feature_order, feature_importance_array):
+    plt.barh(
+        [f"feature_{i}" for i in feature_order],
+        feature_importance_array,
+        color="skyblue",
+    )
+    plt.xlabel("Feature Importance")
+    plt.ylabel("Features")
+    plt.title("PDP-based Feature Importance")
+    plt.tight_layout()
