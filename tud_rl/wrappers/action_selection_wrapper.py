ff80c81886accfa1dba8206f13c93c87cf9ac22d
Jonas Keller <jonas.keller1@mailbox.tu-dresden.de>
2024-07-07 13:30:33 +0200

    fixed function wrapper

--- tud_rl/wrappers/action_selection_wrapper.py
+++ tud_rl/wrappers/action_selection_wrapper.py
@@ -4,7 +4,6 @@ import numpy as np
 from ixai.utils.wrappers.base import Wrapper
 
 
-
 class ActionSelectionWrapper(Wrapper):
     def __init__(
         self,
@@ -33,20 +32,14 @@ class ActionSelectionWrapper(Wrapper):
         x_input = self.convert_1d_input_to_arr(x)
         output = self._prediction_function(x_input)
         return self.convert_arr_output_to_dict(output)
-    
-class ActionSelectionWrapperALE():
-    def __init__(self, action_selection_function, feature_of_interest = None) -> None:
-        # self.action_selection_function = np.vectorize(action_selection_function)
+
+
+class ActionSelectionWrapperALE:
+    def __init__(self, action_selection_function) -> None:
         self.action_selection_function = action_selection_function
 
-    def __call__(self, x: typing.Union[typing.List[dict], dict]) -> typing.Any:
-        output = np.zeros_like(x)
+    def __call__(self, x: np.ndarray) -> typing.Any:
+        output = np.zeros((np.shape(x)[0], 1))
         for i in range(np.shape(x)[0]):
-            output[i,:] = self.action_selection_function(x[i,:])
-            
-            # np.append(output, self.action_selection_function(data_point))
-
-
-        
-        
+            output[i, :] = self.action_selection_function(x[i, :])
         return output
